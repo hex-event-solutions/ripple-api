eba27d2bc1cc66f7ff815eaff9b05962219fd5c9
Ollie Nye <ollie@nyemail.co.uk>
2020-06-02 13:46:45 +0100

    Rubocop

--- app/models/crew.rb
+++ app/models/crew.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Crew < ApplicationRecord
+  has_many :crew_roles
+  has_many :roles, through: :crew_roles
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Role < ApplicationRecord
+  has_many :crew_roles
+  has_many :crew, through: :crew_roles
 end
