5d785c332b9cd72d53fadd92088dd7e4abbfafb0
Ollie Nye <ollie@nyemail.co.uk>
2020-06-09 17:49:41 +0100

    Remove graphql models for roles

--- app/graphql/types/ripple_crew_role_type.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Types
-  class RippleCrewRoleType < Types::BaseObject
-    field :crew, Types::RippleCrewType, null: false
-    field :role, Types::RippleRoleType, null: false
-  end
-end
