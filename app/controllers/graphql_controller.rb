b64b91f38b9f1bed947cd4cc9146a724b806da6d
Ollie Nye <ollie@nyemail.co.uk>
2020-07-17 10:20:54 +0100

    Enforce requirement of oauth token when querying GraphQL

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -10,9 +10,21 @@ class GraphqlController < ApplicationController
     variables = ensure_hash(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
+
+    company_id = nil
+    if request.env['keycloak.user_groups'].nil?
+      company_id = params[:company_id] if params.include? :company_id
+    else
+      company_id = request.env['keycloak.user_groups'].first.split('/')[1]
+      ripple_admin = request.env['keycloak.user_groups'].include? '/ripple-admins'
+    end
+
     context = {
-      company_id: request.env['keycloak.groups']&.first.split('.')[0]
+      company_id: company_id,
+      ripple_admin: ripple_admin,
+      user_roles: request.env['keycloak.user_roles']
     }
+
     result = AssetmanagementSchema.execute(
       query,
       variables: variables,
@@ -55,4 +67,16 @@ class GraphqlController < ApplicationController
 
     render json: { errors: [{ message: message, backtrace: backtrace }], data: {} }, status: 500
   end
+
+  # def invalid_foreign_key(err)
+  #   puts 'in rescue block!'
+  #   table = err.message.match(/on table \\"([a-z_].+)\\"\\n/)&.captures&.first
+
+  #   not_found_msg = 'This object is still referenced, but the referenced table could not be identified'
+  #   raise GraphQL::ExecutionError, not_found_msg if table.nil?
+
+  #   qualifier = %w[a e i o u].include? table[0].downcase ? 'an' : 'a'
+
+  #   raise GraphQL::ExecutionError, "This object is still referenced by #{qualifier} #{table.humanize.titleize}"
+  # end
 end
