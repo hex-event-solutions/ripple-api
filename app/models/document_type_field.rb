c30e00c44a40e5ed8920924aeba1aa401bf5de45
Ollie Nye <ollie@nyemail.co.uk>
2020-07-17 10:19:05 +0100

    Remove the concept of documenttypefields

--- app/models/document_type_field.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class DocumentTypeField < ApplicationRecord
-  belongs_to :company
-  belongs_to :document_type
-
-  validates :company, :name, :resource, :property, :document_type, presence: true
-  validates :name, length: { maximum: 32 }
-end
