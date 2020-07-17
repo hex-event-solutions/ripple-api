0869844fc1ff0f12594383d9e0ff0f577d14269a
Ollie Nye <ollie@nyemail.co.uk>
2020-07-17 10:21:31 +0100

    Add helpers to ApplicationRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,24 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def currency(value)
+    "£#{format('%<value>.2f', value: value)}"
+  end
+
+  def percentage(value)
+    value != 0 ? "#{format('%<value>.1f', value: value)}%" : ''
+  end
+
+  def address
+    components = [
+      address1,
+      address2,
+      address3,
+      city,
+      county,
+      postcode
+    ]
+    components.compact.join("\n")
+  end
 end
