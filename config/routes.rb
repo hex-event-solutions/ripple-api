c3b0790e6744561d580e3d427ab9b419f11317df
Ollie Nye <ollie@nyemail.co.uk>
2020-08-12 14:24:48 +0100

    Add probe and website endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'test', to: 'mustache#test'
+  get '/website/template', to: 'website#template'
+  get '/website/component/:name', to: 'website#component'
+  get '/website/styles', to: 'website#styles'
+
+  get '/probe', to: 'probe#probe'
+
+  # get 'test', to: 'mustache#test'
 end
