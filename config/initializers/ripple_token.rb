d21f6ef499386313e1d9994c5a7eb44bfe30c6c2
Ollie Nye <ollie@nyemail.co.uk>
2020-08-12 17:01:22 +0100

    Make test environment an explicit check

--- config/initializers/ripple_token.rb
+++ config/initializers/ripple_token.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
-RippleToken::Client.configure do |c|
-  c.base_url = ENV.fetch('KEYCLOAK_BASE_URL', '')
-  c.realm = ENV.fetch('KEYCLOAK_REALM', '')
-  c.public_key_ttl = ENV.fetch('KEYCLOAK_PUBLIC_KEY_TTL', 86_400)
-  c.public_paths = {
-    'GET' => [%r{/website}, %r{/probe}],
-    'POST' => [%r{/graphql}],
-    put: [],
-    patch: [],
-    delete: []
-  }
+if Rails.env != 'test'
+  RippleToken::Client.configure do |c|
+    c.base_url = ENV.fetch('KEYCLOAK_BASE_URL')
+    c.realm = ENV.fetch('KEYCLOAK_REALM')
+    c.public_key_ttl = ENV.fetch('KEYCLOAK_PUBLIC_KEY_TTL', 86_400)
+    c.public_paths = {
+      'GET' => [%r{/website}, %r{/probe}],
+      'POST' => [%r{/graphql}],
+      put: [],
+      patch: [],
+      delete: []
+    }
+  end
 end
