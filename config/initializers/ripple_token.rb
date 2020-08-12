45b385e4d49782fb526894ff834f2e91d7f6c172
Ollie Nye <ollie@nyemail.co.uk>
2020-08-12 14:24:35 +0100

    Allow website and probe to be executed without providing a JWT token

--- config/initializers/ripple_token.rb
+++ config/initializers/ripple_token.rb
@@ -5,8 +5,8 @@ RippleToken::Client.configure do |c|
   c.realm = ENV.fetch('KEYCLOAK_REALM')
   c.public_key_ttl = ENV.fetch('KEYCLOAK_PUBLIC_KEY_TTL', 86_400)
   c.public_paths = {
-    get: [],
-    'POST' => ['/graphql'],
+    'GET' => [%r{/website}, %r{/probe}],
+    'POST' => [%r{/graphql}],
     put: [],
     patch: [],
     delete: []
