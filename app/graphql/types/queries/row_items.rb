a208bfe0d1d14043366f902a3b92410acb67c501
Ollie Nye <ollie@nyemail.co.uk>
2020-08-18 10:15:12 +0100

    Get row item descriptions and prices for a description for autocomplete

--- app/graphql/types/queries/row_items.rb
+++ app/graphql/types/queries/row_items.rb
@@ -5,10 +5,22 @@ module Types
     module RowItems
       def self.included(base)
         base.field :row_items, [Types::Ripple::RowItemType], null: false, description: 'Returns all row items'
+        base.field :row_item_descriptions, [String], null: false, description: 'All variants on the row item description'
+        base.field :row_item_prices, [Float], null: false, description: 'Prices for a given row item' do
+          argument :description, String, required: true
+        end
       end
 
       def row_items
         RowItem.where(company_id: context[:company_id]).without_templates
+      end
+
+      def row_item_descriptions
+        RowItem.where(company_id: context[:company_id]).without_templates.map(&:description).uniq
+      end
+
+      def row_item_prices(description:)
+        RowItem.where(company_id: context[:company_id], description: description).map(&:price)
       end
     end
   end
