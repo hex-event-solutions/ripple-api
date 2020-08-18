0da1d2de1dae384571625b1dfc22c5f57d9346d3
Ollie Nye <ollie@nyemail.co.uk>
2020-08-18 10:14:39 +0100

    Get one client query

--- app/graphql/types/queries/clients.rb
+++ app/graphql/types/queries/clients.rb
@@ -5,10 +5,17 @@ module Types
     module Clients
       def self.included(base)
         base.field :clients, [Types::Ripple::ClientType], null: false, description: 'Returns all clients'
+        base.field :client, Types::Ripple::ClientType, null: false, description: 'A client' do
+          argument :id, Types::BaseObject::ID, required: true
+        end
       end
 
       def clients
         Client.where(company_id: context[:company_id]).without_templates
+      end
+
+      def client(id:)
+        Client.find_by!(company_id: context[:company_id], id: id)
       end
     end
   end
